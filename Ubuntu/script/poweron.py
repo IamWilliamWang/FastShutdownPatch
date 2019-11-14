eca5bc59cdcc0969d246acba43c0023366c0f7e4
William Wang <iamjerichoholic@hotmail.com>
2019-11-14 19:38:48 +0800

    小数点后只保留三位数

--- Ubuntu/script/poweron.py
+++ Ubuntu/script/poweron.py
@@ -1,9 +1,9 @@
 from datetime import datetime
 
 dt = datetime.now()
-power_on_text = 'INSERT INTO [Table](开机时间) VALUES (GETDATE())'.replace('GETDATE()',
-                                                                       "'" + dt.strftime('%Y-%m-%d %H:%M:%S.%f') + "'")
+power_on_text = 'INSERT INTO [Table](开机时间) VALUES (GETDATE())'.replace(
+    'GETDATE()', "'" + dt.strftime('%Y-%m-%d %H:%M:%S.%f')[:-3] + "'")
 power_on_text += '鋝'
-with open('TimeDatabase.cache', 'a') as f:
+with open('TimeDatabase.cache', 'a', encoding='utf-8') as f:
     f.write(power_on_text)
 print('插入开机时间成功！')
